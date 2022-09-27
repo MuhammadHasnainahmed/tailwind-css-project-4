bf177eaa737734678d6ca7283905d3df9ac515ae
= <=>
2022-09-27 12:40:24 +0500

    all commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+
+    extend: {
+      colors:{
+        electricblue:'#80FFFF',
+        electricblueDark:'#00e6e6',
+      },
+    },
+  },
+  plugins: [],
+}
